c2f97b25fb419ea3dd640dc102009a01df9dd782
agnynureza <rezaagny@gmail.com>
2020-07-29 01:00:05 +0700

    pekan 2 : hari 2

--- Pekan 2/Tugas hari ke - 2/index2.js
+++ Pekan 2/Tugas hari ke - 2/index2.js
@@ -1,17 +1,25 @@
 var readBooksPromise = require('./promise.js')
- 
-var books = [
-    {name: 'LOTR', timeSpent: 3000}, 
-    {name: 'Fidas', timeSpent: 2000}, 
-    {name: 'Kalkulus', timeSpent: 4000}
+
+var books = [{
+        name: 'LOTR',
+        timeSpent: 3000
+    },
+    {
+        name: 'Fidas',
+        timeSpent: 2000
+    },
+    {
+        name: 'Kalkulus',
+        timeSpent: 4000
+    }
 ]
- 
+
 // Lanjutkan code untuk menjalankan function readBooksPromise
-for(let i=0 ; i < books.length ; i ++) {
-    readBooksPromise(1000,books[i]).then(function(fulfill){
-        console.log(fulfill)
-    })
-    .catch(function(err){
-        console.log(err)
-    })
+for (let i = 0; i < books.length; i++) {
+    readBooksPromise(1000, books[i]).then(function (fulfill) {
+            console.log(fulfill)
+        })
+        .catch(function (err) {
+            console.log(err)
+        })
 }
