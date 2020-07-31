47a77c8c0592272136b11f9c9f7d14c2b4712415
agnynureza <rezaagny@gmail.com>
2020-08-01 00:44:32 +0700

    pekan 2 : hari 5

--- Pekan 2/Tugas Hari ke - 5/tugas10/src/App.js
+++ Pekan 2/Tugas Hari ke - 5/tugas10/src/App.js
@@ -1,24 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="borders">
+			<div id="border">
+				<div id="main-content">
+					<h1>Form Pembelian Buah</h1>
+					<form action="" id="table-buah">
+						<table>
+							<tbody>
+								<tr>
+									<td><label><strong>Nama Pelanggan</strong></label></td>
+                	<td><input style={{width:'100%'}} type="text"/></td>
+								</tr>
+								<tr>
+									<td id="daftar-item"><label><strong>Daftar Item</strong></label>
+									</td>
+									<td>
+												<input type="checkbox" value="semangka"/>
+												<label>Semangka</label><br/>
+												<input type="checkbox" value="jeruk"/>
+												<label>Jeruk</label><br/>
+												<input type="checkbox" value="nanas"/>
+												<label>nanas</label><br/>
+												<input type="checkbox" value="salak"/>
+												<label>salak</label><br/>
+												<input type="checkbox" value="anggur"/>
+												<label>anggur</label><br/>
+									</td>
+								</tr>
+							</tbody>
+						</table>
+						<input className='submit-buah' type="submit" value="Kirim"/>
+					</form>
+				</div>
+			</div>
     </div>
   );
 }
